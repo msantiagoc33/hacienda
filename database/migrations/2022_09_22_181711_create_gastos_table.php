8a065b3edd55ae3dff88557700e5fdd77398ba8c
Manuel Santiago Cabeza <msantiagoc33@gmail.com>
2022-09-22 18:53:36 +0200

    actualizado con las tablas

---
+++ database/migrations/2022_09_22_181711_create_gastos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gastos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('concepto_id');
+            $table->date('fecha');
+            $table->double('importe', 8, 2);
+            $table->string('comentario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gastos');
+    }
+};
---
+++ database/migrations/2022_09_22_181731_create_historicos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('historicos', function (Blueprint $table) {
+            $table->id();           
+            $table->integer('plataforma_id');
+            $table->integer('cliente_id');
+            $table->smallInteger('huespedes');
+            $table->date('fechaEntrada');
+            $table->date('fechaSalida');
+            $table->double('importe', 8, 2);
+            $table->string('comentario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('historicos');
+    }
+};
